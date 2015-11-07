0fe2653d075f8f364bbcaf97d21c349a3bda9a8c
Julian Pitney <julianpitney@gmail.com>
2015-11-07 14:41:53 -0500

    Assignment 2: committing data files

--- Midterm/has_no_e.py
+++ Midterm/has_no_e.py
@@ -1,16 +1,58 @@
-void has_no_e(str var1)
-	x = len(var1)
-	y = 0
-	
-	while y < x:
-		if var1[y] == 'e'
-			return True
-		
-		y += 1
-
-	return False
+def has_no_e(in_string):
+	for letter in in_string:
+		if letter == 'e':
+			return False
+	return True
 
 
+print()
+test_str = "t*st string without that charact*r"
+print(test_str)
+print(has_no_e(test_str))
+print()
+test_str2 = "test string with that character"
+print(test_str2)
+print(has_no_e(test_str2))
+print()
+print("===================================================================")
+print("          \\\\ gadsby_stripped.txt 'e' character check //          ||")
+print("           ===========================================           ||")
+print("                                                                 ||")
+print("      	      Prints 'TRUE' if line contains no 'e'              ||")
+print("               Prints 'FALSE' if line contains 'e'               ||")
+print("                                                                 ||")
+print("          Also prints number of lines with and w/o               ||")
+print("               'e' at the bottom of all output.                  ||")
+print("                                                                 ||")
+print("NOTE:  We are considering lines to be strings delimited by       ||")
+print("       whatever python's newline character is and not lines      ||")
+print("       as they might appear in a text editor.                    ||")
+print("===================================================================")
+
+print()
 
 
 
+
+
+
+
+line_number = 1 
+occurrences_of_e = 0
+occurrences_without_e = 0
+
+for line in open('gadsby_stripped.txt'):
+	print("Line Number [" + str(line_number) + "]: " + str(has_no_e(line)))
+	if(has_no_e(line) == False):
+		occurrences_of_e += 1
+	elif(has_no_e(line) == True):
+		occurrences_without_e += 1
+
+	line_number += 1
+
+print()
+print("LINES WITHOUT 'e' =  " + str(occurrences_without_e))
+print("LINES WITH 'e' =  " + str(occurrences_of_e))
+
+
+# Working
--- Midterm/strip.sh
+++ Midterm/strip.sh
@@ -8,12 +8,12 @@
 touch "temp1"
 head -n -$2  $3 > "temp1" 
 touch "temp2"
-tail -n +$1 "temp1" > "temp2"
-
+tail -n +$1 "temp1" > "temp2"    
 rm "temp1"
+cat "temp2"
+rm "temp2"
 
-
-# now just output contents of temp 2 to stdout but i don't know how with 5 mins left
+# working
 
 
 
